c8f9654967b8eefa466776ea06c330f0f699caa2
shazaib munawar <shazaib424647@gmail.com>
2022-07-08 12:23:18 +0500

    updates

--- main.py
+++ main.py
@@ -2,35 +2,7 @@
 # Get request at "http://localhost:5000/api/v1/tableData" expect to recieve table data in json format
 
 from flask import Flask, jsonify
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
 import json
-
-url = "https://www.govcagecodes.com/?code=&company=TESLA#results"
-
-try:
-    tableHeaders = []
-
-    source = requests.get(url).text
-
-    soup = BeautifulSoup(source, "lxml")
-    table = soup.find("table", id="rt")
-
-    for td in table.thead.tr.find_all("td"):
-        tableHeaders.append(td.text)
-
-    df = pd.DataFrame(columns=tableHeaders)
-
-    for tr in table.tbody.find_all("tr"):
-        data = tr.find_all("td")
-        tableRows = [td.text for td in data]
-        length = len(df)
-        df.loc[length] = tableRows
-
-    df.to_json("./tableData.json")
-except Exception as err:
-    print(err)
 
 app = Flask(__name__)
 
