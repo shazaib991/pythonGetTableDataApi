f66e70591936c49fd2c521f30d96c96c7961a271
shazaib munawar <shazaib424647@gmail.com>
2022-07-13 14:44:37 +0500

    update

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 # Get request at "http://localhost:5000/api/v1/tableData" expect to recieve table data in json format
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/v1/tableData")
 def getTableData():
